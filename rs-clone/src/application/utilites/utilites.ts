509fe57026e1edcdc9ddf8ec98240e15db414caa
DianaSmertina <am.dianasmertina@gmail.com>
2023-02-06 14:43:46 +0400

    refactor: change createDocElem on createOurElement make it easier to merge

--- rs-clone/src/application/utilites/utilites.ts
+++
@@ -1,8 +0,0 @@
-export function createDocElement(tag: string, className: string, text?: string) {
-    const el = document.createElement(tag);
-    el.className = className;
-    if (text) {
-        el.innerText = text;
-    }
-    return el;
-}
