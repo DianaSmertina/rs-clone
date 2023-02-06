18f378ee5a440b9330da6c757efdb26d47384785
DianaSmertina <am.dianasmertina@gmail.com>
2023-02-07 00:31:17 +0400

    feat: add submit handler or sighUp btn

--- rs-clone/src/application/pages/components/modal-window.ts
+++ rs-clone/src/application/pages/components/modal-window.ts
@@ -1,11 +1,14 @@
 import Component from '../../patterns/component';
 import { createOurElement } from '../../patterns/createElement';
+import { Api } from '../../server/server-api';
 
 export class ModalWindow extends Component {
     constructor(tagName: string, className: string, private btnText: string) {
         super(tagName, className);
         this.btnText = btnText;
     }
+
+    private api = new Api();
 
     private createInput(text: string) {
         const div = createOurElement('div', 'input-wrap');
@@ -15,6 +18,9 @@ export class ModalWindow extends Component {
         if (text === 'Пароль') {
             input.setAttribute('type', 'password');
             input.setAttribute('pattern', '^\\S{5}\\S+$');
+            input.classList.add('password');
+        } else {
+            input.classList.add('login');
         }
         div.append(label, input);
         return div;
@@ -26,9 +32,19 @@ export class ModalWindow extends Component {
         btn.setAttribute('type', 'submit');
         const greetings = createOurElement('h3', 'form-wrap__greetings');
         const text = createOurElement('p', 'form-wrap__text');
+        let submitRes: string;
         if (this.btnText === 'Регистрация') {
             greetings.innerText = 'Рады приветсвовать нового знатока стран!';
             text.innerText = 'Придумай логин и пароль, длина которого должна быть 6 или больше символов';
+            form.addEventListener('submit', async (e) => {
+                e.preventDefault();
+                const password = form.querySelector<HTMLInputElement>('.password')?.value;
+                const login = form.querySelector<HTMLInputElement>('.login')?.value;
+                if (password && login) {
+                    submitRes = await this.api.signUp({ username: login, password: password });
+                    console.log(submitRes);
+                }
+            });
         } else {
             greetings.innerText = 'Рады видеть тебя снова!';
             text.innerText = 'Пожалуйста, введи логин и пароль от своей учетной записи';
---
+++ rs-clone/src/application/server/server-api.ts
@@ -0,0 +1,22 @@
+export class Api {
+    private static base = 'http://localhost:5000/api'; // поменять, когда будет задеплоеный сервер
+
+    async signIn(data: { username: string; password: string }): Promise<string> {
+        const response = await fetch(`${Api.base}/sign-in`, {
+            method: 'GET',
+            body: JSON.stringify(data),
+        });
+        return response.json();
+    }
+
+    async signUp(data: { username: string; password: string }) {
+        const response = await fetch(`${Api.base}/user`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        return response.json();
+    }
+}
