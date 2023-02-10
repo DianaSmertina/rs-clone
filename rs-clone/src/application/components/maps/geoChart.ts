d3e58d93750c111cb01b0ad526d7b541a6754e01
DianaSmertina <am.dianasmertina@gmail.com>
2023-02-10 18:38:16 +0400

    feat: start population quiz

--- rs-clone/src/application/components/maps/geoChart.ts
+++ rs-clone/src/application/components/maps/geoChart.ts
@@ -1,19 +1,11 @@
-export function drawChart(container: HTMLElement) {
+export function drawChart(container: HTMLElement, countryData: Array<Array<string | number>>) {
     google.charts.load('current', {
         callback: drawRegionsMap,
         packages: ['geochart'],
     });
     google.charts.setOnLoadCallback(drawRegionsMap);
     function drawRegionsMap() {
-        const data = google.visualization.arrayToDataTable([
-            ['Country', 'Popularity'],
-            ['Germany', 200],
-            ['United States', 300],
-            ['Brazil', 400],
-            ['Canada', 500],
-            ['France', 600],
-            ['RU', 700],
-        ]);
+        const data = google.visualization.arrayToDataTable(countryData);
         const options = {};
         const chart = new google.visualization.GeoChart(container);
         chart.draw(data, options);
