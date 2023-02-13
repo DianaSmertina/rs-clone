1bd346f154fe840eae16e0d484b7a3ab2af63f2e
DianaSmertina <am.dianasmertina@gmail.com>
2023-02-13 23:27:04 +0400

    refactor: separate createCheckBtn method

--- rs-clone/src/application/pages/quizzes/population-quiz/component.ts
+++ rs-clone/src/application/pages/quizzes/population-quiz/component.ts
@@ -39,13 +39,39 @@ export class PopulationQuestion {
         return geoChartWrap;
     }
 
+    private createCheckBtn(answerBtns: HTMLElement[], answers: Icountry[]) {
+        const checkBtn = createOurElement('button', 'btn btn__bordered btn__check-population', 'Проверить');
+        checkBtn.setAttribute('disabled', 'disabled');
+
+        checkBtn.addEventListener('click', () => {
+            answerBtns.forEach((btn, i) => {
+                btn.innerText === answers[i].countryRu
+                    ? btn.classList.add('btn__right')
+                    : btn.classList.add('btn__wrong');
+
+                const rightPopulation = (
+                    Number(answers.find((el) => el.countryRu === btn.innerText)?.population) / 1000000
+                ).toFixed(2);
+
+                btn.innerText += ` ${rightPopulation} млн`;
+            });
+            checkBtn.setAttribute('disabled', 'disabled');
+            document.querySelector('.btn__next')?.removeAttribute('disabled');
+        });
+
+        return checkBtn;
+    }
+
     private createAnswerBlock() {
         const answersBlock = createOurElement('div', 'answers flex-rows', '');
         const answers = this.mapData.slice(0);
+
         const answerBtns = answers?.map(() => {
             const answer = createOurElement('button', 'btn btn__population');
             answer.addEventListener('click', () => {
                 answer.innerText = '';
+                const checkBtn = document.querySelector('.btn__check-population');
+
                 if (!answerBtns.every((btn) => btn.innerText) && checkBtn) {
                     checkBtn.setAttribute('disabled', 'disabled');
                 }
@@ -54,23 +80,7 @@ export class PopulationQuestion {
             return answer;
         });
 
-        const checkBtn = createOurElement('button', 'btn btn__bordered btn__check-population', 'Проверить');
-        checkBtn.setAttribute('disabled', 'disabled');
-        checkBtn.addEventListener('click', () => {
-            answerBtns.forEach((btn, i) => {
-                btn.innerText === answers[i].countryRu
-                    ? btn.classList.add('btn__right')
-                    : btn.classList.add('btn__wrong');
-                const rightPopulation = (
-                    Number(answers.find((el) => el.countryRu === btn.innerText)?.population) / 1000000
-                ).toFixed(2);
-                btn.innerText += ` ${rightPopulation} млн`;
-            });
-            checkBtn.setAttribute('disabled', 'disabled');
-            document.querySelector('.btn__next')?.removeAttribute('disabled');
-        });
-
-        answersBlock.append(checkBtn);
+        answersBlock.append(this.createCheckBtn(answerBtns, answers));
         return answersBlock;
     }
 
